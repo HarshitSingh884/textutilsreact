3f8da923957a99d38ca4f9279a613990dea322e9
HarshitSingh884 <harshitsinghnaps307@gmail.com>
2022-04-12 17:58:45 +0530

    Updates

---
+++ precache-manifest.b357ea0178cf2ed7c052cd00b89eb371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e06076aa12b9030a673",
+    "url": "/textutilsreact/static/css/main.c7ff6f51.chunk.css"
+  },
+  {
+    "revision": "5e06076aa12b9030a673",
+    "url": "/textutilsreact/static/js/main.5e06076a.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/textutilsreact/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/textutilsreact/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "ff2ed35a30ed583e61cb",
+    "url": "/textutilsreact/static/js/runtime~main.ff2ed35a.js"
+  },
+  {
+    "revision": "ef81525870175ddd4041a41fd965c9df",
+    "url": "/textutilsreact/index.html"
+  }
+];
